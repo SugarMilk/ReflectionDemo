3a0357bc5d777ffc55005feb59a3854dc2b01df8
huangjian <huangjian@bojoydeiMac.local>
2016-12-01 14:21:04 +0800

    First Commit

---
+++ ReflectionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReflectionDemo
+//
+//  Created by huangjian on 16/12/1.
+//  Copyright © 2016年 bojoy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
